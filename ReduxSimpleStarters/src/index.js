51dae5bbafcb9f1c77538b4bd7d6c5b2ed7da56d
Thomas Fowler <tkfowler13@gmail.com>
2017-09-13 15:49:16 -0700

    init search bar and video list

--- ReduxSimpleStarters/src/index.js
+++ ReduxSimpleStarters/src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+const API_KEY = 'AIzaSyATH-NgNJJ7u3PMEBgCxW2OCATebqRvyjo';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //ES6 version of this.setState({ videos: videos })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
