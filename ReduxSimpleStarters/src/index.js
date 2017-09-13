af0827f9fff087ec30b2408a4a866f0bea227440
Thomas Fowler <tkfowler13@gmail.com>
2017-09-13 16:11:49 -0700

    video player added. suggested videos clickable and change primary video

--- ReduxSimpleStarters/src/index.js
+++ ReduxSimpleStarters/src/index.js
@@ -3,24 +3,33 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyATH-NgNJJ7u3PMEBgCxW2OCATebqRvyjo';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-      //ES6 version of this.setState({ videos: videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     })
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     )
   }
