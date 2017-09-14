219d76526af8a2345296dfd4eb5426f86ab131dd
Thomas Fowler <tkfowler13@gmail.com>
2017-09-13 17:30:26 -0700

    made search bar functional

--- ReduxSimpleStarters/src/index.js
+++ ReduxSimpleStarters/src/index.js
@@ -15,20 +15,25 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     })
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.selectedVideo}/>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
-          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
           videos={this.state.videos} />
       </div>
     )
