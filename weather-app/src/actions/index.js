bd9ad85cd7c7416f4e8c725018538d9939968302
Thomas Fowler <tkfowler13@gmail.com>
2017-09-15 14:46:10 -0700

    init action creator. api call to weather forcast

---
+++ weather-app/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'b255f248bd738294cf039be1be82570d';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
