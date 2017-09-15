6a44b3bc8da85e9bbe079bab89c053d84b438ddc
Thomas Fowler <tkfowler13@gmail.com>
2017-09-15 15:05:14 -0700

    fixed api url

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'b255f248bd738294cf039be1be82570d';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
