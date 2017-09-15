3fa439571178827ba657a1d8670a35304608d143
Thomas Fowler <tkfowler13@gmail.com>
2017-09-15 10:58:01 -0700

    action creator, added dispatch to  to container

--- books-redux/src/containers/book-list.js
+++ books-redux/src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { selectBook } from  '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title}  className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+            {book.title}
+        </li>
       );
     });
   }
@@ -26,4 +33,12 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned form thsi func will end up as props on BookList container
+function mapDispatchToProps(dispatch) {
+  //whenver selectBook is called, result should be passed to all reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+//promote BookList form Component to container - needs to know about
+//dispatch method, selectBook. Make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
