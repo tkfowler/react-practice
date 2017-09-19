bf2058c2978b4b5f78f1b5d2133ea60c45af4400
Thomas Fowler <tkfowler13@gmail.com>
2017-09-18 17:00:28 -0700

    render list of posts

---
+++ blog/src/components/posts_index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
+// { fetchPosts } == { fetchPosts: fetchPosts }
+// instead of def. a separate func. mapDispatchToProps, can set second arg as the function
+// use separate func format when you want to do computation on how you want to call
+// action creator ahead of time.
